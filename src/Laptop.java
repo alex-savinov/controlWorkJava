5c27cab8002ee6b980266ddeb82679222c88b1e4
Alex <sp.savinov@gmail.com>
2023-10-15 08:52:13 +0300

    End

---
+++ src/Laptop.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+public class Laptop {
+    private Integer ram;
+    private Integer memory;
+    private Double diagonal;
+    private Integer weight;
+    private String name;
+    private String manufacturer;
+    private String processor;
+    public Laptop() {
+    }
+    public Laptop(String name,String manufacturer,String processor,Double diagonal,Integer ram,Integer memory,Integer weight) {
+        this.name = name;
+        this.manufacturer = manufacturer;
+        this.processor = processor;
+        this.diagonal =  Math.round(diagonal * 10.0) / 10.0;
+        this.ram = ram;
+        this.memory = memory;
+        this.weight = weight;
+    }
+
+    public Integer getRam(){return ram;};
+    public  Integer getMemory(){return memory;};
+    public  Double getDiagonal(){return diagonal;};
+    public  Integer getWeight(){return weight;};
+    public  String getName(){return name;};
+    public  String getManufacturer(){return manufacturer;};
+    public  String getProcessor(){return processor;};
+
+
+    public Set<Laptop> Filter(Set<Laptop> laptop) {
+        Scanner scan = new Scanner(System.in);
+        Set<Laptop> listLaptop = new HashSet<>(laptop);
+
+        System.out.println("Укажите номер или номера критериев фильрации ноутбуков" +
+                "\n1. Диагональ\n2. Объем ОЗУ\n3. Размер жесткого диска");
+        String userRequest = scan.nextLine();
+
+        for (int i = 0; i < userRequest.length(); i++) {
+            if(1 == Character.getNumericValue(userRequest.charAt(i))) {
+                System.out.println("Укажите искомую диагональ");
+                String enterDiagonal = scan.nextLine();
+                double doubleParseEnterDiagonal = Double.parseDouble(enterDiagonal);
+                for (Laptop tempLaptop : laptop) {
+                    if (doubleParseEnterDiagonal != tempLaptop.diagonal){
+                        listLaptop.remove(tempLaptop);
+                    }
+                }
+            }
+            if(2 == Character.getNumericValue(userRequest.charAt(i))) {
+                System.out.println("Укажите нужный объем ОЗУ");
+                String enterRam = scan.nextLine();
+                int intParseEnterRam = Integer.parseInt(enterRam);
+                for (Laptop tempLaptop : laptop) {
+                    if (intParseEnterRam != tempLaptop.ram){
+                        listLaptop.remove(tempLaptop);
+                    }
+                }
+            }
+            if(3 == Character.getNumericValue(userRequest.charAt(i))) {
+                System.out.println("Укажите нужный размер жесткого диска");
+                String enterMemory = scan.nextLine();
+                int intParseEnterMemory = Integer.parseInt(enterMemory);
+                for (Laptop tempLaptop : laptop) {
+                    if (intParseEnterMemory != tempLaptop.memory){
+                        listLaptop.remove(tempLaptop);
+                    }
+                }
+            }
+        }
+        return listLaptop;
+    }
+
+
+
+
+
+    @Override
+    public String toString() {
+        return   String.format(
+                "\n\nНазвание: %s \n" +
+                        "Производитель: %s \n" +
+                        "Процессор: %s\n" +
+                        "Диагональ: %.1f\n" +
+                        "ОЗУ: %d\n" +
+                        "Жесткий диск: %d\n" +
+                        "Вес: %d",
+                name, manufacturer, processor, diagonal, ram, memory, weight);
+
+    }
+}
